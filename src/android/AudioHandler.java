8f62bd3b2eb62f6f23442a6520b02b3e05cc6aaf
Aaron Newman <aaron@cxtsoftware.com>
2016-09-26 13:36:47 -0700

    Change Audio focus gain

--- src/android/AudioHandler.java
+++ src/android/AudioHandler.java
@@ -427,7 +427,7 @@ public class AudioHandler extends CordovaPlugin {
                 case (AudioManager.AUDIOFOCUS_LOSS) :
                     pauseAllLostFocus();
                     break;
-                case (AudioManager.AUDIOFOCUS_GAIN):
+                case (AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK):
                     resumeAllGainedFocus();
                     break;
                 default:
@@ -442,7 +442,7 @@ public class AudioHandler extends CordovaPlugin {
         AudioManager am = (AudioManager) this.cordova.getActivity().getSystemService(Context.AUDIO_SERVICE);
         int result = am.requestAudioFocus(focusChangeListener,
                                           AudioManager.STREAM_MUSIC,
-                                          AudioManager.AUDIOFOCUS_GAIN);
+                                          AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
 
         if (result != AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
             LOG.e(TAG2,result + " instead of " + AudioManager.AUDIOFOCUS_REQUEST_GRANTED);
