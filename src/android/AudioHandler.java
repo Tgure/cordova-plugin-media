18ed3581ebc1c3b0eeaf2bcdf13dc89bfbd4a1db
Aaron Newman <aaron@cxtsoftware.com>
2016-09-26 14:24:19 -0700

    Change Audio focus

--- src/android/AudioHandler.java
+++ src/android/AudioHandler.java
@@ -423,7 +423,7 @@ public class AudioHandler extends CordovaPlugin {
             public void onAudioFocusChange(int focusChange) {
                 switch (focusChange) {
                 case (AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) :
-                    restoreVolume();
+                    pauseAllLostFocus();
                     break;
                 case (AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) :
                 case (AudioManager.AUDIOFOCUS_LOSS) :
